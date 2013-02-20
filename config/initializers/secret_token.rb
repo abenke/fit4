58ffea8b67d2bc59aae64490515e923b2d1bef20
Alex Benke <alex@betterment.com>
2013-02-19 22:09:03 -0500

    first base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fit4::Application.config.secret_token = '3fdffe47a5c64d359dfccf4cba4a04fc09112431371989d50e3640af789e6e898791ca150ba85a0c40e58cab78795eddf3d5dc016489193f3dd8ea045159d9fd'
